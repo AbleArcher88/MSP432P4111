9080871ff839ded8cc359ca3fbc834464c048aba
AbleArcher88 <lmastrce@AcaComp.home>
2022-10-23 16:26:59 -0500

    example now compiles

--- examples/led_blinker.rs
+++ examples/led_blinker.rs
@@ -8,7 +8,17 @@ use panic_halt as _;
 #[entry]
 fn main() -> ! {
     let periph = msp432p4111::Peripherals::take().unwrap();
+    let wdt = periph.WDT_A;
+    let pgpio = periph.DIO;
 
-    let dio = periph.DIO;
+    wdt.wdtctl.write(|w| {
+        unsafe {
+            w.wdtpw().bits(0x5A);
+        }
+        w.wdthold().bit(true);
+    });
+
+
+
     loop {}
 }
