16170198b8c39e8ebf1816a95ed3cbc5348909e5
AbleArcher88 <lmastrce@AcaComp.home>
2022-10-22 21:10:33 -0500

    Broke apart lib.rs to increase readability

---
+++ src/ref_a.rs
@@ -0,0 +1,1 @@
+# [doc = r"Register block"] # [repr (C)] pub struct RegisterBlock { # [doc = "0x00 - REF Control Register 0"] pub refctl0 : REFCTL0 , } # [doc = "REFCTL0 (rw) register accessor: an alias for `Reg<REFCTL0_SPEC>`"] pub type REFCTL0 = crate :: Reg < refctl0 :: REFCTL0_SPEC > ; # [doc = "REF Control Register 0"] pub mod refctl0 ;
